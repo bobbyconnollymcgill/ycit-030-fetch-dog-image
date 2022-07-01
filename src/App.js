1b3f04359012fd78b4322146ff24329fc3381435
Bobby Connolly <bobbyconnolly@bobbys-mbp.lan>
2022-06-30 21:26:17 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function App() {
+    console.log("rerendering...")
+
+    const [state, setState] = useState(null)
+
+    const fetchDogImage = () => {
+        fetch("https://random.dog/woof.json", { method: "GET" })
+            .then((response) => {
+                console.log(response)
+
+                return response.json()
+            })
+            .then((data) => {
+                console.log(data)
+
+                setState(data)
+            })
+    }
+
+    useEffect(() => {
+        fetchDogImage()
+    }, [])
+
+    useEffect(() => {
+        if (!state) {
+            return
+        }
+        if (state.fileSizeBytes > 4161408) {
+            setState({ error: "image-too-big" })
+        }
+    }, [state])
+
+    return (
+        <div className="App">
+            <button
+                onClick={() => {
+                    fetchDogImage()
+                }}
+            >
+                Get dog image
+            </button>
+            <div>
+                {!!state && !state.error && (
+                    <img src={state.url} style={{ width: "300px" }} />
+                )}
+                {!!state && state.error && <div>{state.error}</div>}
+            </div>
+        </div>
+    )
 }
-
-export default App;
